f32208c7fe18579f34d7df4c2a4b19cfcb9a05a7
Dongmin Lee <medi8600@gmail.com>
2016-04-21 18:50:52 -0500

    Made CRUD function works!

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -6,4 +6,29 @@ class ToolsController < ApplicationController
   def show
     @tool = Tool.find(params[:id])
   end
+
+  def new
+    @tool = Tool.new
+  end
+
+  def create
+    @tool = Tool.create(:name => params[:tool]["name"], :price => params[:tool]["price"], :quantity => params[:tool]["quantity"])
+    redirect_to tool_path(@tool)
+  end
+
+  def edit
+    @tool = Tool.find(params[:id])
+  end
+
+   def update
+    @tool = Tool.find(params[:id])
+    @tool.update_attributes(:name => params[:tool]["name"], :price => params[:tool]["price"], :quantity => params[:tool]["quantity"])
+    redirect_to tool_path
+  end
+
+  def destroy
+    @tool = Tool.find(params[:id])
+    @tool.delete
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root 'tools#index'
   get "/tools", to: "tools#index", as: "tools"
+  get "/tools/new", to: "tools#new", as: "new_tool"
+  get "/tools/:id", to: "tools#show", as: "tool"
+  post "/tools", to: "tools#create"
+  get "/tools/:id/edit", to: "tools#edit", as: "edit_tool"
+  patch "/tools/:id", to: "tools#update"
+  put "/tools/:id", to: "tools#update"
+  delete "/tools/:id", to: "tools#destroy"
 end
 
